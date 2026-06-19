9c78faa49d42c6931547827ab1f0e6bec3b5041b
Melanie Carrillo <melanieycarrillo23@gmail.com>
2026-06-19 11:18:33 -0500

    fix: ajustar vistas permisos y procedimientos de base de datos

--- database/10_pruebas.sql
+++ database/10_pruebas.sql
@@ -1,6 +1,151 @@
--- LLAMADAS 
+-- =========================================================
+-- SCRIPT 10 - PRUEBAS
+-- Proyecto: Tienda de ropa online
+-- =========================================================
 
 BEGIN;
+
+-- =========================================================
+-- DATOS DE PRUEBA: CLIENTE, ADMIN Y SUPERADMIN
+-- =========================================================
+
+CALL registrar_cliente(
+    'Cliente Prueba',
+    '3124567890',
+    'cliente.prueba@gmail.com',
+    'hash_cliente_123',
+    'M',
+    '2000-05-10'
+);
+
+CALL registrar_usuario_admin(
+    'Admin Prueba',
+    '3134567890',
+    'admin.prueba@gmail.com',
+    'hash_admin_123',
+    'F',
+    '1998-03-15',
+    (SELECT rol_id FROM roles WHERE nombre = 'ADMIN')
+);
+
+CALL registrar_usuario_admin(
+    'Superadmin Prueba',
+    '3144567890',
+    'superadmin.prueba@gmail.com',
+    'hash_superadmin_123',
+    'O',
+    '1995-08-20',
+    (SELECT rol_id FROM roles WHERE nombre = 'SUPERADMIN')
+);
+
+
+-- =========================================================
+-- DATOS DE PRUEBA: PRODUCTOS
+-- =========================================================
+
+CALL registrar_producto(
+    'Camiseta deportiva prueba',
+    55000,
+    (SELECT cat_id FROM categorias WHERE nombre = 'Superior'),
+    (SELECT est_id FROM estilos WHERE nombre = 'Deportivo')
+);
+
+CALL registrar_producto(
+    'Jean clásico prueba',
+    90000,
+    (SELECT cat_id FROM categorias WHERE nombre = 'Inferior'),
+    (SELECT est_id FROM estilos WHERE nombre = 'Casual')
+);
+
+
+-- =========================================================
+-- DATOS DE PRUEBA: INVENTARIO
+-- =========================================================
+
+CALL registrar_inventario(
+    (SELECT pro_id FROM productos WHERE nombre = 'Camiseta deportiva prueba'),
+    20,
+    (SELECT tal_id FROM tallas WHERE nombre = 'M'),
+    (SELECT col_id FROM colores WHERE nombre = 'Negro')
+);
+
+CALL registrar_inventario(
+    (SELECT pro_id FROM productos WHERE nombre = 'Jean clásico prueba'),
+    15,
+    (SELECT tal_id FROM tallas WHERE nombre = 'L'),
+    (SELECT col_id FROM colores WHERE nombre = 'Azul')
+);
+
+
+-- =========================================================
+-- PRUEBA: EDITAR PRODUCTO, CAMBIAR ESTADO Y ACTUALIZAR INVENTARIO
+-- =========================================================
+
+CALL editar_producto(
+    (SELECT pro_id FROM productos WHERE nombre = 'Jean clásico prueba'),
+    'Jean clásico prueba actualizado',
+    95000,
+    (SELECT cat_id FROM categorias WHERE nombre = 'Inferior'),
+    (SELECT est_id FROM estilos WHERE nombre = 'Casual')
+);
+
+CALL cambiar_estado_producto(
+    (SELECT pro_id FROM productos WHERE nombre = 'Jean clásico prueba actualizado'),
+    TRUE
+);
+
+CALL actualizar_inventario(
+    (
+        SELECT i.inv_id
+        FROM inventarios i
+        INNER JOIN productos p ON p.pro_id = i.pro_id
+        WHERE p.nombre = 'Camiseta deportiva prueba'
+        LIMIT 1
+    ),
+    25
+);
+
+
+-- =========================================================
+-- DATOS DE PRUEBA: VENTA, DETALLE Y PAGO
+-- Se crean mediante la función de carrito
+-- =========================================================
+
+SELECT realizar_compra_carrito(
+    (SELECT per_id FROM personas WHERE correo = 'cliente.prueba@gmail.com'),
+    (SELECT met_id FROM metodos_pago WHERE nombre = 'PSE'),
+    jsonb_build_array(
+        jsonb_build_object(
+            'inv_id',
+            (
+                SELECT i.inv_id
+                FROM inventarios i
+                INNER JOIN productos p ON p.pro_id = i.pro_id
+                WHERE p.nombre = 'Camiseta deportiva prueba'
+                LIMIT 1
+            ),
+            'cantidad',
+            2
+        ),
+        jsonb_build_object(
+            'inv_id',
+            (
+                SELECT i.inv_id
+                FROM inventarios i
+                INNER JOIN productos p ON p.pro_id = i.pro_id
+                WHERE p.nombre = 'Jean clásico prueba actualizado'
+                LIMIT 1
+            ),
+            'cantidad',
+            1
+        )
+    )
+);
+
+
+-- =========================================================
+-- LLAMADOS A FUNCIONES
+-- =========================================================
 
 SELECT fn_validar_correo('cliente@gmail.com');
 SELECT fn_validar_telefono('3124567890');
@@ -8,36 +153,35 @@ SELECT fn_validar_stock(0);
 SELECT fn_estado_producto(0);
 SELECT fn_estado_producto(10);
 
-CALL registrar_inventario(1, 10, 1, 1);
-CALL aumentar_stock(1, 5);
 
-SELECT realizar_compra_carrito(
-    1,
-    1,
-    '[
-        {"inv_id": 1, "cantidad": 2},
-        {"inv_id": 3, "cantidad": 1}
-    ]'::jsonb
-);
+-- =========================================================
+-- LLAMADOS A VISTAS PRINCIPALES
+-- =========================================================
 
-UPDATE productos
-SET precio = precio + 1000
-WHERE pro_id = 1;
-
-SELECT * FROM aud_productos;
-
-UPDATE inventarios
-SET stock = stock + 1
-WHERE inv_id = 1;
-
-SELECT * FROM aud_inventarios;
-
-SELECT * FROM vw_inventario_simple;
-SELECT * FROM vw_catalogo_productos_detalle;
-SELECT * FROM vw_catalogo_productos;
 SELECT * FROM vw_admin_productos;
-SELECT * FROM vw_resumen_ventas;
 SELECT * FROM vw_admin_inventario;
+SELECT * FROM vw_resumen_ventas;
+SELECT * FROM vw_detalle_ventas_admin;
+SELECT * FROM vw_pedidos_cliente;
+SELECT * FROM vw_detalle_pedido_cliente;
+SELECT * FROM vw_usuarios_sistema;
+SELECT * FROM vw_auditoria_general;
+
+
+-- =========================================================
+-- LLAMADOS A VISTAS PARAMÉTRICAS
+-- =========================================================
+
+SELECT * FROM vw_categorias;
+SELECT * FROM vw_estilos;
+SELECT * FROM vw_tallas;
+SELECT * FROM vw_colores;
+SELECT * FROM vw_metodos_pago;
+
+
+-- =========================================================
+-- REFRESCAR Y CONSULTAR VISTA MATERIALIZADA
+-- =========================================================
 
 REFRESH MATERIALIZED VIEW mv_resumen_ventas_productos;
 SELECT * FROM mv_resumen_ventas_productos;
